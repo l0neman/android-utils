a24f8a28347e20abd5ba7cf11e311f8eb6056ea4
wangruning <wa123fei@gmail.com>
2019-01-19 16:17:24 +0800

    rename error package.

--- utilslib/src/main/java/com/runing/utilslib/general/concurrent/ExecutorCreator.java
+++ utilslib/src/main/java/com/runing/utilslib/general/concurrent/ExecutorCreator.java
@@ -1,4 +1,4 @@
-package com.runing.utilslib.general.conurrent;
+package com.runing.utilslib.general.concurrent;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
